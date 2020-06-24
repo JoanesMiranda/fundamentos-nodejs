5316855719430fa6978dbdc6cad79a41ea868021
Joanes Souto <joanes.souto@gmail.com>
2020-06-24 18:06:36 -0300

    desafio fundamentos nodejs concluido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,7 +6,14 @@ interface Balance {
   total: number;
 }
 
+interface createTransactionTDO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
+
   private transactions: Transaction[];
 
   constructor() {
@@ -14,16 +21,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    const { income, outcome } = this.transactions.reduce(
+      (acumulation: Balance, transaction: createTransactionTDO) => {
+        switch (transaction.type) {
+          case 'income':
+            acumulation.income += transaction.value;
+            break;
+          case 'outcome':
+            acumulation.outcome += transaction.value;
+          default:
+            break;
+        }
+        return acumulation;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      }
+    );
+
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+
+  public create({ title, value, type }: createTransactionTDO): Transaction {
+
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
+
 }
 
 export default TransactionsRepository;
